1a0ada224fab45e607a986a40c5f3237da7747ac
ak792 <ark73@georgetown.edu>
2015-03-07 12:50:30 -0500

    completed

---
+++ Ch4/Hypnosister/BNRHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.h
+//  Hypnosister
+//
+//  Created by Alex Keyes on 3/6/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
---
+++ Ch4/Hypnosister/BNRHypnosisView.m
@@ -0,0 +1,31 @@
+//
+//  BNRHypnosisView.m
+//  Hypnosister
+//
+//  Created by Alex Keyes on 3/6/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRHypnosisView.h"
+
+@implementation BNRHypnosisView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
