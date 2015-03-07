255d731e0a9ebdb4f969f4c25776fd88d64f703b
ak792 <ark73@georgetown.edu>
2015-03-07 15:29:36 -0500

    completed challenges

---
+++ Ch6/challenges/HypnoNerd Bronze/BNRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRQuizViewController.h
+//  Quiz
+//
+//  Created by Alex Keyes on 3/5/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRQuizViewController : UIViewController
+
+@end
