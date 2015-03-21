a18b0251b4f9e36efbf3a6ccb50c10a778e1cdb6
ak792 <ark73@georgetown.edu>
2015-03-21 16:28:43 -0400

    finished challenges

---
+++ Ch15/challenges/Quiz Silver/Quiz/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Quiz
+//
+//  Created by Alex Keyes on 3/5/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
